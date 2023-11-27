14ead3c9ea79884219bf569b6f76cc4079173019
Felipe Alves Leão de Araújo <82793324+FelipeAlvesLeao@users.noreply.github.com>
2023-11-27 20:17:04 -0300

    Create Evento.java

---
+++ docs/models/Evento.java
@@ -0,0 +1,28 @@
+import org.springframework.data.annotation.Id;
+
+
+public class Evento {
+
+  @Id
+  public String id;
+  public String nome;
+  public String descricao;
+  public String endereco;
+  @JsonFormat(pattern="yyyy-MM-dd")
+  public Date dataInicial;
+  
+  public Evento() {}
+
+//  Criar um construtor aqui com todos os parametros que são required
+// public Evento(params) {
+ //   this.nome = nome;
+//  }
+
+  @Override
+  public String toString() {
+    return String.format(
+        "Customer[id=%s, nome='%s', descricao='%s']",
+        id, nome, descricao);
+  }
+
+}
